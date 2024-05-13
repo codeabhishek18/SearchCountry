a7ff4518c2c2f537ed7f6809fd5d589e894c12e3
Abhishek Magadum <codeabhishek18@gmail.com>
2024-05-13 17:42:57 +0530

    Updated styles

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import styles from './Countries.module.css';
+import './Countries.css';
 
 const Countries = () =>
 {
@@ -29,12 +29,12 @@ const Countries = () =>
     const filteredCountries = name === '' ? countries : [...countries].filter((country) => country.name.common.toLowerCase().includes(name))
 
     return(
-        <div className={styles.container}>
-            <input type="text" className={styles.searchbar} placeholder='Search for countries' onChange={(e)=>setName(e.target.value)}/>
-            <div className={styles.wrapper}>
+        <div className='container'>
+            <input type='text' className='searchbar' placeholder='Search for countries' onChange={(e)=>setName(e.target.value)}/>
+            <div className='wrapper'>
                 {filteredCountries.map((country)=>
                 (
-                <div className={styles.countryCard} key={country.name.common}>
+                <div className='countryCard' key={country.name.common}>
                     <img src={country.flags.png} alt={country.flags.alt}/>
                     <p>{country.name.common}</p>
                 </div>
